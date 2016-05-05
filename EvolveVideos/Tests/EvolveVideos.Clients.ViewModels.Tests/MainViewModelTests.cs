9e5a6f50c98c37c6ed82f0cbd8a4f97f10397fd6
cjgaliana <cjgaliana@gmail.com>
2016-05-05 16:39:13 +0100

    Added dummy tests to set up the CI server

---
+++ EvolveVideos/Tests/EvolveVideos.Clients.ViewModels.Tests/MainViewModelTests.cs
@@ -0,0 +1,27 @@
+﻿
+
+using NUnit.Framework;
+
+namespace EvolveVideos.Clients.ViewModels.Tests
+{
+    [TestFixture]
+    public class MainViewModelTests
+    {
+        [Test]
+        public void PassingTest()
+        {
+            Assert.AreEqual(4, Add(2, 2));
+        }
+
+        [Test]
+        public void FailingTest()
+        {
+            Assert.AreEqual(5, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
